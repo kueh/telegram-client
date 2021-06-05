7d8ebffedd7351571bced1265e3cea408813d405
Yadunand Prem <yadunand@yadunut.com>
2021-06-05 12:52:48 +0800

    feat: static link

--- build.rs
+++ build.rs
@@ -1,6 +1,32 @@
+use cmake::Config;
 
 fn main() {
-  println!("cargo:rustc-link-search=native=./lib");
-  println!("cargo:rustc-link-lib=dylib=tdjson");
-}
+    let tdjson_libs = [
+        "tdjson_static",
+        "tdjson_private",
+        "tdclient",
+        "tdcore",
+        "tdapi",
+        "tdactor",
+        "tddb",
+        "tdsqlite",
+        "tdnet",
+        "tdutils",
+    ];
 
+    let dst = Config::new("td")
+        .profile("Release")
+        .very_verbose(true)
+        .build();
+    println!("cargo:rustc-link-search={}/lib", dst.display());
+
+    for lib in tdjson_libs.iter() {
+        println!("cargo:rustc-link-lib=static={}", lib);
+    }
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=ssl");
+    println!("cargo:rustc-link-lib=dylib=crypto");
+    println!("cargo:rustc-link-lib=dylib=dl");
+    println!("cargo:rustc-link-lib=dylib=z");
+    println!("cargo:rustc-link-lib=dylib=m");
+}
